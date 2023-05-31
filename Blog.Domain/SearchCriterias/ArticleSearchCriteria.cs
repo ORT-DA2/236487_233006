822d6707920fe422d3085ba9abe177c639ff7690
Agustin <agustin.j.nogueira@gmail.com>
2023-05-31 16:07:21 -0300

    added filter articles by author id

--- Blog.Domain/SearchCriterias/ArticleSearchCriteria.cs
+++ Blog.Domain/SearchCriterias/ArticleSearchCriteria.cs
@@ -6,9 +6,12 @@ namespace Blog.Domain.SearchCriterias
     {
         public string? q { get; set; }
 
+        public int? authorId { get; set; }
+
         public Expression<Func<Article, bool>> Criteria()
         {
-            return a => string.IsNullOrEmpty(q) || a.Title.IndexOf(q) >= 0 || a.Content.IndexOf(q) >= 0;
+            return a => (string.IsNullOrEmpty(q) || a.Title.IndexOf(q) >= 0 || a.Content.IndexOf(q) >= 0) &&
+                        (authorId == null || a.Author.Id == authorId);
         }
     }
 }
