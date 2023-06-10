fed6a4b687fa3511010c3b165846391ab4b9d2db
Agustin <agustin.j.nogueira@gmail.com>
2023-06-09 23:11:34 -0300

    filter comments by isApproved and isRejected

--- server/Blog.Domain/SearchCriterias/CommentSearchCriteria.cs
+++ server/Blog.Domain/SearchCriterias/CommentSearchCriteria.cs
@@ -6,9 +6,13 @@ namespace Blog.Domain.SearchCriterias
     {
         public int? ArticleId { get; set; }
 
+        public bool? IsApproved { get; set; }
+
+        public bool? IsRejected { get; set; }
+
         public Expression<Func<Comment, bool>> Criteria()
         {
-            return c => ArticleId == null || c.Author.Id == ArticleId;
+            return c => (ArticleId == null || c.Author.Id == ArticleId) && (IsApproved == null || c.IsApproved == IsApproved) && (IsRejected == null || c.IsRejected == IsRejected);
         }
     }
 }
